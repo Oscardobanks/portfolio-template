f06e8aaf9f148e15dc3146fc83681b8853c417dc
Oscardobanks <takurob2003@gmail.com>
2025-08-09 02:19:02 +0100

    fixed build error

--- components/InfiniteSpinner.tsx
+++ components/InfiniteSpinner.tsx
@@ -6,7 +6,7 @@ import Marquee from "react-fast-marquee";
 const InfiniteSpinner = () => {
     const items = Array.from({ length: 6 }, (_, index) => index)
     return (
-        <div className="py-[300px] -my-6 bg-[url('/bg.svg')] bg-cover bg-bottom bg-no-repeat rotate-180 ">
+        <div className="py-[300px] -my-6 bg-[url('/images/bg.svg')] bg-cover bg-bottom bg-no-repeat rotate-180 ">
             {/* Desktop Layout */}
             <div className="hidden lg:flex justify-between items-center gap-4 rotate-180 px-20">
                 {items.map((_, index) => (
